81d8728de8a5aaca34868cdee6491811aac4db26
Nick Chavez <nickchavez@google.com>
2019-07-17 14:29:20 -0400

    add tODO

--- pizza_order/main.py
+++ pizza_order/main.py
@@ -8,11 +8,16 @@ jinja_env = jinja2.Environment(
     autoescape=True)
 
 # Page where user can make an order for pizza.
-class OrderHandler(webapp2.RequestHandler):
+class TakeOrderHandler(webapp2.RequestHandler):
     # Take the user's order.
     def get(self):
         template = jinja_env.get_template('templates/take_order.html')
         self.response.write(template.render())
+
+# Page where user can view the order they've submitted.
+class SubmitOrderHandler(webapp2.RequestHandler):
+    # TODO
+    pass
 
 # Page with info about our Pizza Service.
 class AboutHandler(webapp2.RequestHandler):
@@ -20,8 +25,10 @@ class AboutHandler(webapp2.RequestHandler):
         self.response.write('This pizza does not stain your keyboard!')
 
 app = webapp2.WSGIApplication([
-  # Route localhost:8080/ to OrderHandler.
-  ('/', OrderHandler),
+  # Route localhost:8080/ to TakeOrderHandler.
+  ('/', TakeOrderHandler),
+  # Route localhost:8080/submit_order to SubmitOrderHandler.
+  ('/submit_order', SubmitOrderHandler),
   # Route localhost:8080/about to AboutHandler.
   ('/about', AboutHandler),
 ], debug=True)
