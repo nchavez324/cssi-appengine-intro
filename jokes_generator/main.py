76b56e0636ae0730a6440faadcca4bc9d10eaf97
Nick Chavez <nickchavez@google.com>
2019-07-17 14:08:41 -0400

    Fix errors.

--- jokes_generator/main.py
+++ jokes_generator/main.py
@@ -4,29 +4,30 @@ import webapp2
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=["jinja2.ext.autoescape"],
+    extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class WelcomePage(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template('welcome.html')
         template_vars = {
-            "greeting": "Welcome!",
-            "available_qualities": ["corny", "good", "overused"],
+            'greeting': 'Welcome!',
+            'available_qualities': ['corny', 'good', 'overused'],
         }
         self.response.write(template.render(template_vars))
 
 class ResultPage(webapp2.RequestHandler):
     def get(self):
-        template = the_jinja_environment.get_template('result.html')
+        template = jinja_env.get_template('result.html')
         template_vars = {
-            "joke_line_1": "If Cinderella's shoe was a perfect fit",
-            "joke_line_2": "Then why did it fall off?",
-            "image_url": "/static/thatsthejoke.jpg",
+            'joke_line_1': 'If Cinderella\'s shoe was a perfect fit',
+            'joke_line_2': 'Then why did it fall off?',
+            'image_url': '/static/thatsthejoke.jpg',
+            'joke_is_good': True,
         }
         self.response.write(template.render(template_vars))
 
 app = webapp2.WSGIApplication([
-    ("/", WelcomePage),
-    ("/result", ResultPage),
+    ('/', WelcomePage),
+    ('/result', ResultPage),
 ], debug=True)
